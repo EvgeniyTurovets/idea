899972843fa4e53920a2e9fcfbf8662335ea6f99
EvgeniyTurovets <websafrasoft@gmail.com>
2021-09-13 10:02:24 +0300

    1
    1

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,15 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function () {
-  $('.add-to-basket').on('click', function () {
-    console.log('1');
+$(function () {
+  $('#burger-btn').on('click', function () {
+    $('.header-nav-dropdown').show();
+  });
+  $('#burger-close').on('click', function () {
+    $('.header-nav-dropdown').hide();
+  });
+  $('.catalog-nav-link-arrow').on('click', function (e) {
+    e.preventDefault();
+    $(this).closest('.catalog-nav-dropdown').toggleClass('active');
   });
 });
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    $('.add-to-basket').on('click', function () {
-        console.log('1')
+$(function(){
+    $('#burger-btn').on('click', function(){
+        $('.header-nav-dropdown').show()
+    })
+
+    $('#burger-close').on('click', function(){
+        $('.header-nav-dropdown').hide()
+    })
+
+    $('.catalog-nav-link-arrow').on('click', function(e){
+        e.preventDefault()
+        $(this).closest('.catalog-nav-dropdown').toggleClass('active')
     })
 })
